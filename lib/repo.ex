7b1beb0f500a7ffc9bf191918f7d180bb2edc93c
Miguel C <miquecg@users.noreply.github.com>
2022-03-19 19:47:26 +0100

    Use atom keys on internal data map

--- lib/repo.ex
+++ lib/repo.ex
@@ -6,21 +6,21 @@ defmodule HackerNews.Repo do
 
   @top_stories [
     %{
-      "by" => "dhouston",
-      "descendants" => 71,
-      "id" => 8863,
-      "kids" => [
+      by: "dhouston",
+      descendants: 71,
+      id: 8863,
+      kids: [
         8952,
         9224,
         8917,
         8884,
         8887
       ],
-      "score" => 111,
-      "time" => 1_175_714_200,
-      "title" => "My YC app: Dropbox - Throw away your USB drive",
-      "type" => "story",
-      "url" => "http://www.getdropbox.com/u/2/screencast.html"
+      score: 111,
+      time: 1_175_714_200,
+      title: "My YC app: Dropbox - Throw away your USB drive",
+      type: "story",
+      url: "http://www.getdropbox.com/u/2/screencast.html"
     }
   ]
 
