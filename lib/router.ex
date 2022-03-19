3cf445fd282d6e15cccfeb050d24db63c3b30ccf
Miguel C <miquecg@users.noreply.github.com>
2022-03-19 17:57:25 +0100

    Do not extract to a render function yet

--- lib/router.ex
+++ lib/router.ex
@@ -8,18 +8,14 @@ defmodule HackerNews.Router do
 
   get "/stories" do
     stories = HackerNews.get_top_stories()
-    render_json(conn, stories)
-  end
-
-  match _ do
-    send_resp(conn, 404, "Not Found")
-  end
-
-  defp render_json(conn, stories) do
-    data = HackerNewsWeb.StoryView.render("collection.json", stories)
+    data = StoryView.render("collection.json", stories)
 
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(200, data)
+  end
+
+  match _ do
+    send_resp(conn, 404, "Not Found")
   end
 end
