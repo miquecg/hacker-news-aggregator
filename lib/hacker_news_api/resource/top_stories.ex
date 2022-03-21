f0c07c28a63d1c7a525be10a1f274bb3b7f6ff3b
Miguel C <miquecg@users.noreply.github.com>
2022-03-21 18:57:35 +0100

    Create API resource TopStories

---
+++ lib/hacker_news_api/resource/top_stories.ex
@@ -0,0 +1,39 @@
+defmodule HackerNewsApi.Resource.TopStories do
+  @moduledoc """
+  Top stories.
+  """
+
+  alias HackerNewsApi.DataParser
+
+  defstruct [:url]
+
+  @opaque t :: %__MODULE__{
+            url: String.t()
+          }
+
+  @type option :: {:scheme, String.t()} | {:path, String.t()}
+  @type opts :: [option]
+
+  @defaults [scheme: "https", path: "/v0/topstories"]
+
+  @doc """
+  Creates a `__MODULE__` struct.
+
+  Defaults provided:
+  - scheme: "https"
+  - path: resource path
+
+  They can be overridden passing a second
+  argument of `t:opts/0`.
+  """
+  @spec new(String.t(), opts) :: t
+  def new(host, opts \\ []) do
+    opts = Keyword.merge(@defaults, opts)
+    {:ok, url} = DataParser.parse_url(host, opts)
+    struct(__MODULE__, url: to_string(url))
+  end
+
+  defimpl HackerNewsApi.HttpResource do
+    def request(%{url: url}), do: {:get, url, []}
+  end
+end
