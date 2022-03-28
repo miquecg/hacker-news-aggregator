8d49839744400a5f1722a7f992bd355f3abca382
Miguel C <miquecg@users.noreply.github.com>
2022-03-28 15:58:47 +0200

    Group all parse_url/2 tests

---
+++ test/hacker_news_api/data_parser_test.exs
@@ -0,0 +1,54 @@
+defmodule HackerNewsApi.DataParserTest do
+  use ExUnit.Case, async: true
+
+  alias HackerNewsApi.DataParser, as: Parser
+
+  describe "parse_url/2" do
+    test "returns error parsing URL without scheme" do
+      assert {:error, :missing_scheme} = Parser.parse_url("", [])
+      assert {:error, :missing_scheme} = Parser.parse_url("example.com", [])
+      assert {:error, :missing_scheme} = Parser.parse_url("example.com", path: "/foo")
+    end
+
+    test "returns error parsing URL without path" do
+      assert {:error, :missing_path} = Parser.parse_url("http://", [])
+      assert {:error, :missing_path} = Parser.parse_url("http://example.com", [])
+    end
+
+    test "returns error parsing URL without host" do
+      assert {:error, :missing_host} = Parser.parse_url("http://", path: "/foo")
+      assert {:error, :missing_host} = Parser.parse_url("http:///foo", [])
+    end
+
+    test "returns URL with scheme" do
+      assert {:ok, uri = %URI{}} = Parser.parse_url("example.com/foo", scheme: "http")
+      assert to_string(uri) == "http://example.com/foo"
+    end
+
+    test "returns URL with path" do
+      assert {:ok, uri = %URI{}} = Parser.parse_url("http://example.com", path: "/foo")
+      assert to_string(uri) == "http://example.com/foo"
+
+      assert {:ok, uri = %URI{}} = Parser.parse_url("http://example.com", path: "bar")
+      assert to_string(uri) == "http://example.com/bar"
+    end
+
+    test "returns URL with scheme and path" do
+      assert {:ok, uri = %URI{}} = Parser.parse_url("example.com", scheme: "http", path: "/foo")
+      assert to_string(uri) == "http://example.com/foo"
+    end
+
+    test "params are defaults and do not override" do
+      assert {:ok, uri = %URI{}} = Parser.parse_url("http://example.com/foo", scheme: "ftp")
+      assert to_string(uri) == "http://example.com/foo"
+
+      assert {:ok, uri = %URI{}} = Parser.parse_url("http://example.com/foo", path: "/bar")
+      assert to_string(uri) == "http://example.com/foo"
+    end
+
+    test "removes double slashes" do
+      assert {:ok, uri = %URI{}} = Parser.parse_url("http://example.com/", path: "/foo")
+      assert to_string(uri) == "http://example.com/foo"
+    end
+  end
+end
