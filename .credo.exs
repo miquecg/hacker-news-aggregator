f73a59eeaae26e81415c17a387b7fbb62ccf89a1
Miguel C <miquecg@users.noreply.github.com>
2022-03-28 15:51:58 +0200

    Disable Credo.Check.Consistency.ExceptionNames

---
+++ .credo.exs
@@ -0,0 +1,25 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      files: %{
+        included: [
+          "lib/",
+          "test/"
+        ],
+        excluded: [
+          ~r"/_build/",
+          ~r"/deps/"
+        ]
+      },
+      plugins: [],
+      requires: [],
+      strict: false,
+      parse_timeout: 5000,
+      color: true,
+      checks: [
+        {Credo.Check.Consistency.ExceptionNames, false}
+      ]
+    }
+  ]
+}
