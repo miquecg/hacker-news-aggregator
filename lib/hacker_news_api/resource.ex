3163454ddbe432ccf4fb85a02b9e8d6d377ff4a9
Miguel C <miquecg@users.noreply.github.com>
2022-03-31 17:19:11 +0200

    Add path params handling to BaseResource

--- lib/hacker_news_api/resource.ex
+++ lib/hacker_news_api/resource.ex
@@ -24,37 +24,84 @@ defmodule HackerNewsApi.BaseResource do
   with `Kernel.use/2`.
   """
 
+  alias HackerNewsApi.{DataParser, Error}
+
   @host Application.compile_env!(:hacker_news, [:api, :host])
 
   @doc false
   defmacro __using__(opts) do
     path = Keyword.fetch!(opts, :path)
-    url = to_string(new!(@host, opts))
+    url = to_string(build_url!(@host, opts))
 
+    [
+      quote do
+        defstruct [:url]
+
+        @opaque t :: %__MODULE__{
+                  url: String.t()
+                }
+
+        @path unquote(path)
+
+        @spec path :: String.t()
+        def path, do: @path
+
+        defimpl HackerNewsApi.Resource do
+          def request(%{url: url}), do: {:get, url, []}
+        end
+      end,
+      quote_new(path, url)
+    ]
+  end
+
+  defp quote_new(path, url) when is_binary(path) do
+    {:ok, path_params} = DataParser.parse_path_params(path)
+    quote_new(path_params, url)
+  end
+
+  defp quote_new([], url) do
     quote do
-      defstruct [:url]
-
-      @opaque t :: %__MODULE__{
-                url: String.t()
-              }
-
-      @spec new :: t
+      @spec new :: {:ok, t}
       def new do
-        struct(__MODULE__, url: unquote(url))
-      end
-
-      @path unquote(path)
-
-      @spec path :: String.t()
-      def path, do: @path
-
-      defimpl HackerNewsApi.Resource do
-        def request(%{url: url}), do: {:get, url, []}
+        {:ok, struct(__MODULE__, url: unquote(url))}
       end
     end
   end
 
-  alias HackerNewsApi.{DataParser, Error}
+  defp quote_new(path_params, url) do
+    quote do
+      import HackerNewsApi.BaseResource,
+        only: [replace_path_params: 3, build_error: 2]
+
+      @spec new(keyword()) :: {:ok, t} | {:error, Error.Params.t()}
+      def new(args) do
+        if contains?(args, unquote(path_params)) do
+          url = replace_path_params(unquote(url), unquote(path_params), args)
+          {:ok, struct(__MODULE__, url: url)}
+        else
+          {:error, build_error(:missing_path_params, args)}
+        end
+      end
+
+      defp contains?(args, path_params) do
+        Enum.all?(path_params, fn <<?:, param::binary>> ->
+          Keyword.has_key?(args, String.to_atom(param))
+        end)
+      end
+    end
+  end
+
+  defmacro build_error(error, params) do
+    module = __CALLER__.module
+
+    quote do
+      %Error.Params{
+        module: unquote(module),
+        params: unquote(params),
+        error: unquote(error)
+      }
+    end
+  end
 
   @typep host :: String.t()
   @type option :: {:scheme, String.t()} | {:path, String.t()}
@@ -71,8 +118,8 @@ defmodule HackerNewsApi.BaseResource do
   Defaults provided:
   - scheme: "https"
   """
-  @spec new(host, opts) :: ok | error
-  def new(host, opts \\ []) do
+  @spec build_url(host, opts) :: ok | error
+  def build_url(host, opts \\ []) do
     opts = Keyword.merge(@defaults, opts)
 
     case DataParser.parse_url(host, opts) do
@@ -84,19 +131,22 @@ defmodule HackerNewsApi.BaseResource do
     end
   end
 
-  @spec new!(host, opts) :: URI.t() | no_return()
-  def new!(host, opts \\ []) do
-    case new(host, opts) do
+  @spec build_url!(host, opts) :: URI.t() | no_return()
+  def build_url!(host, opts \\ []) do
+    case build_url(host, opts) do
       {:ok, url} -> url
       {:error, error} -> raise error
     end
   end
 
-  defp build_error(error, params) do
-    %Error.Params{
-      module: __MODULE__,
-      params: params,
-      error: error
-    }
+  @typep url :: String.t()
+  @typep path_params :: DataParser.path_params()
+
+  @spec replace_path_params(url, path_params, keyword()) :: String.t()
+  def replace_path_params(url, path_params, values) do
+    String.replace(url, path_params, fn <<":", param::binary>> ->
+      key = String.to_atom(param)
+      to_string(values[key])
+    end)
   end
 end
