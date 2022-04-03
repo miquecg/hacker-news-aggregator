4cda12e6ea4526b08b5edc198a89b967bbb8cdcc
Miguel C <miquecg@users.noreply.github.com>
2022-04-03 13:08:58 +0200

    Add retries with exponential backoff to Client

--- lib/hacker_news_api/client.ex
+++ lib/hacker_news_api/client.ex
@@ -5,18 +5,22 @@ defmodule HackerNewsApi.Client do
 
   alias HackerNewsApi.{Client.Response, Error, Resource}
 
-  @adapter Application.compile_env!(:hacker_news, :adapter)
-
-  @type return :: return(Response.t())
-  @type return(t) :: {:ok, t} | {:error, Exception.t()}
+  @type ok_or_error(t) :: {:ok, t} | {:error, Exception.t()}
 
   @typedoc """
   Lowercase type/subtype (e.g. application/json).
   UTF-8 charset assumed.
   """
   @type media_type :: String.t()
-  @type decoder :: (body :: binary() -> return(term()))
-  @type option :: {:decode, {media_type, decoder}}
+  @type decoder :: (body :: binary() -> ok_or_error(term()))
+
+  @type max_retries :: 0..5
+  @type min_delay_ms :: 100..1_000
+  @type max_delay_ms :: 500..5_000
+
+  @type option ::
+          {:decode, {media_type, decoder}}
+          | {:retries, {max_retries, min_delay_ms, max_delay_ms}}
   @type opts :: [option]
 
   @typep resource :: Resource.t()
@@ -26,22 +30,60 @@ defmodule HackerNewsApi.Client do
 
   @typep response :: Response.t()
 
-  @callback do_request(method, url, headers) :: return
+  @callback do_request(method, url, headers) :: ok_or_error(response)
 
-  @spec request(resource, opts) :: return
-  def request(resource, opts \\ []), do: request(@adapter, resource, opts)
+  @adapter Application.compile_env!(:hacker_news, :adapter)
+  @defaults [{:retries, {4, 100, 500}}]
 
-  @spec request(module(), resource, opts) :: return
-  def request(adapter, resource, opts) do
+  @spec request(resource, opts) :: ok_or_error(response)
+  def request(resource, opts \\ []) do
+    opts = Keyword.merge(@defaults, opts)
+    request(@adapter, resource, opts)
+  end
+
+  @typep retries :: non_neg_integer()
+
+  @spec request(module(), resource, opts, retries) :: ok_or_error(response)
+  def request(adapter, resource, opts, retries \\ 0) do
     {method, url, headers} = Resource.request(resource)
 
     case adapter.do_request(method, url, headers) do
+      {:ok, %{status: 429}} -> retry(adapter, resource, opts, retries)
       {:ok, response} -> process_response(response, opts)
       {:error, _} = error -> error
     end
   end
 
-  @spec process_response(response, opts) :: return
+  @spec retry(module(), resource, opts, retries) :: ok_or_error(response)
+  defp retry(adapter, resource, opts, retries) do
+    {max_retries, min_delay, max_delay} = Keyword.fetch!(opts, :retries)
+
+    if retries < max_retries do
+      :ok = backoff(retries, min_delay, max_delay)
+      request(adapter, resource, opts, retries + 1)
+    else
+      error_too_many_requests(resource)
+    end
+  end
+
+  @spec backoff(retries, min_delay_ms, max_delay_ms) :: :ok
+  defp backoff(retries, min_delay, max_delay) do
+    factor = Integer.pow(2, retries)
+    random_delay = Enum.random(min_delay..max_delay)
+    time = factor * random_delay
+
+    Process.send_after(self(), :retry, time)
+
+    receive do
+      :retry -> :ok
+    end
+  end
+
+  defp error_too_many_requests(resource) do
+    {:error, %Error.TooManyRequests{resource: resource}}
+  end
+
+  @spec process_response(response, opts) :: ok_or_error(response)
   defp process_response(response, opts) do
     case Enum.reduce_while(opts, response, &process_with_option/2) do
       %{} = response -> {:ok, response}
@@ -63,10 +105,10 @@ defmodule HackerNewsApi.Client do
   defp match_content_type(response, media_type) do
     case Response.get_media_type(response) do
       {:ok, {^media_type, "utf-8"}} -> :ok
-      {:ok, {_, _} = value} -> media_type_error(unsupported: value)
-      {:error, error} -> media_type_error(error)
+      {:ok, {_, _} = value} -> error_media_type(unsupported: value)
+      {:error, error} -> error_media_type(error)
     end
   end
 
-  defp media_type_error(info), do: {:error, Error.MediaType.exception(info)}
+  defp error_media_type(info), do: {:error, Error.MediaType.exception(info)}
 end
--- test/hacker_news_api/client_test.exs
+++ test/hacker_news_api/client_test.exs
@@ -3,7 +3,7 @@ defmodule HackerNewsApi.ClientTest do
 
   import Plug.Conn
 
-  alias HackerNewsApi.{Client, Client.Response, Resource.TopStories}
+  alias HackerNewsApi.{Client, Client.Response, Error, Resource.TopStories}
 
   setup do
     bypass = Bypass.open()
@@ -12,6 +12,16 @@ defmodule HackerNewsApi.ClientTest do
 
     [bypass: bypass, resource: %TopStories{url: url}]
   end
+
+  @options [
+    {:decode, {"application/json", &Jason.decode/1}},
+    # Max. 4 retries
+    # Min. delay 5 ms
+    # Max. delay 10 ms
+    {:retries, {4, 5, 10}}
+  ]
+
+  defp get_option(key), do: List.keyfind!(@options, key, 0)
 
   @top_stories [
     30_779_046,
@@ -27,7 +37,7 @@ defmodule HackerNewsApi.ClientTest do
       put_json_response(conn, @top_stories)
     end)
 
-    opts = [option(:decode)]
+    opts = [get_option(:decode)]
 
     assert {:ok, %Response{body: @top_stories}} = Client.request(context.resource, opts)
   end
@@ -37,9 +47,42 @@ defmodule HackerNewsApi.ClientTest do
       put_json_response(conn, @top_stories)
     end)
 
-    opts = [option(:decode, {:error, %Jason.DecodeError{}})]
+    decoder = fn _ -> {:error, %Jason.DecodeError{}} end
+    opts = [{:decode, {"application/json", decoder}}]
 
     assert {:error, %Jason.DecodeError{}} = Client.request(context.resource, opts)
+  end
+
+  test "do not retry after one response status 429 (Too Many Requests)", context do
+    Bypass.expect_once(context.bypass, "GET", "/v0/topstories.json", fn conn ->
+      resp(conn, 429, "")
+    end)
+
+    opts = [{:retries, {0, 5, 10}}]
+
+    assert {:error, %Error.TooManyRequests{}} = Client.request(context.resource, opts)
+  end
+
+  test "retry after one response status 429 (Too Many Requests)", context do
+    # Need to do this little trick since Bypass do not support (yet)
+    # setting multiple expectations for the same route.
+    {:ok, agent} = Agent.start_link(fn -> 1 end)
+
+    increment_fn = fn ->
+      Agent.get_and_update(agent, fn step -> {step, step + 1} end)
+    end
+
+    Bypass.expect(context.bypass, "GET", "/v0/topstories.json", fn conn ->
+      case increment_fn.() do
+        # first attempt
+        1 -> resp(conn, 429, "")
+        # retry
+        2 -> put_json_response(conn, @top_stories)
+        _ -> flunk("Got more than one retry")
+      end
+    end)
+
+    assert {:ok, %Response{body: @top_stories}} = Client.request(context.resource, @options)
   end
 
   defp put_json_response(conn, data) do
@@ -49,12 +92,4 @@ defmodule HackerNewsApi.ClientTest do
     |> put_resp_content_type("application/json")
     |> resp(200, body)
   end
-
-  defp option(:decode), do: option(:decode, &Jason.decode/1)
-
-  defp option(:decode, decoder) when is_function(decoder) do
-    {:decode, {"application/json", decoder}}
-  end
-
-  defp option(:decode, value), do: option(:decode, fn _ -> value end)
 end
